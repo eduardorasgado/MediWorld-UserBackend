f051fb25a08a998805670eee05b6dfb96761a0ea
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-07 08:11:49 -0500

    New auditing config class created to activate jpa autiditing

---
+++ src/main/java/com/mediworld/mwuserapi/utils/AuditingConfig.java
@@ -0,0 +1,16 @@
+package com.mediworld.mwuserapi.utils;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * <h1>AuditingConfig</h1>
+ * Clase para activar el auditado de jpa en la aplicacion
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+    //
+}
