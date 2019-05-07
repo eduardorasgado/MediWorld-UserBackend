d16868022cd22fdd32b6d8879d4fa2b545190bd6
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-06 21:48:48 -0500

    paciente service interface: created.

---
+++ src/main/java/com/mediworld/mwuserapi/services/IPacienteService.java
@@ -0,0 +1,46 @@
+package com.mediworld.mwuserapi.services;
+
+
+import com.mediworld.mwuserapi.model.Paciente;
+
+/**
+ * <h1>PacienteService</h1>
+ * Clase que permite traer las transacciones desde el repositorio del modelo de
+ * paciente
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+public interface IPacienteService {
+
+    /**
+     * Metodo para traer un paciente dado su id
+     * @return El paciente deseado
+     */
+    public Paciente getById(String id);
+
+    /**
+     * Metodo para crear un nuevo paciente mediante los datos recibidos en el
+     * frontend
+     * @param paciente los datos del paciente
+     * @return El paciente que se ha creado
+     */
+    public Paciente create(Paciente paciente);
+
+    /**
+     * Metodo que elimina un paciente dada la entidad del paciente que ha sido
+     * previamente buscado
+     * @param paciente
+     * @return
+     */
+    public boolean delete(Paciente paciente);
+
+    /**
+     * Metodo que actualiza un paciente que ya existe dentro de la base de datos
+     * en caso de no existir no actualiza y regresa un null
+     * @param paciente El paciente que va a ser actualizado con los datos que
+     *                 van a ser actualizados
+     * @return un paciente que fue actualizado o null
+     */
+    public Paciente update(Paciente paciente);
+
+}
