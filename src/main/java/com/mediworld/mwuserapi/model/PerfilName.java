5d21f2ad875ce37bad2a1df164aed90055e69c67
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-08 05:12:01 -0500

    Perfil model created. PerfilName enumeration created.

---
+++ src/main/java/com/mediworld/mwuserapi/model/PerfilName.java
@@ -0,0 +1,13 @@
+package com.mediworld.mwuserapi.model;
+
+/**
+ * Representa el perfil que puede tener un paciente dentro de la aplicacion
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+public enum PerfilName {
+    // un paciente normal
+    PACIENTE,
+    // un paciente que actualmente esta vendiendo sus datos
+    PACIENTE_ACTIVE
+}
