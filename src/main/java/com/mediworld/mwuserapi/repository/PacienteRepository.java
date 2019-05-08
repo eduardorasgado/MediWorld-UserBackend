2c5ef46e2910eaff0f41463b07e448f3d2a8c832
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-08 03:05:17 -0500

    Custom methods in paciente repository

--- src/main/java/com/mediworld/mwuserapi/repository/PacienteRepository.java
+++ src/main/java/com/mediworld/mwuserapi/repository/PacienteRepository.java
@@ -4,6 +4,8 @@ import com.mediworld.mwuserapi.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * <h1>PacienteRepository</h1>
  * Repositorio para la transaccion de datos con el modelo de Paciente
@@ -12,5 +14,32 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, String> {
-    //
+
+    /**
+     * Metodo para encontrar un paciente dado su email
+     * @param email
+     * @return Un opcional con paciente
+     */
+    Optional<Paciente> findByEmail(String email);
+
+    /**
+     * Metodo para encontrar un paciente dado su username
+     * @param username
+     * @return Un opcional con paciente
+     */
+    Optional<Paciente> findByUsername(String username);
+
+    /**
+     * Metodo que retorna true o false dada la existencia de un paciente por su username
+     * @param username
+     * @return booleano
+     */
+    Boolean existsByUsername(String username);
+
+    /**
+     * Metodo que retorna un booleano dada la existencia de un paciente por su email
+     * @param email
+     * @return booleano
+     */
+    Boolean existsByEmail(String email);
 }
