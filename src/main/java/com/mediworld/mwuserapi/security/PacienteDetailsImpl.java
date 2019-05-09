285832a4c6b0c74dbc4fc108f74c583d96ab09ef
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-08 23:19:27 -0500

    Data annotation to Perfil model. Paciente details create method and mapping.

--- src/main/java/com/mediworld/mwuserapi/security/PacienteDetailsImpl.java
+++ src/main/java/com/mediworld/mwuserapi/security/PacienteDetailsImpl.java
@@ -1,11 +1,16 @@
 package com.mediworld.mwuserapi.security;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mediworld.mwuserapi.model.Paciente;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * <h1>PacienteDetailsImpl</h1>
@@ -17,11 +22,33 @@ import java.util.Collection;
 public class PacienteDetailsImpl implements UserDetails {
     private String id;
     private String username;
+
     @JsonIgnore
     private String email;
     @JsonIgnore
     private  String password;
     private Collection<? extends GrantedAuthority> authorities;
+
+    /**
+     * Meotodo para crear un paciente con accesos de autoridad
+     * @param paciente Un paciente con todos los datos
+     * @return
+     */
+    public static PacienteDetailsImpl create(Paciente paciente) {
+        // obteniendo las autoridades que representa el paciente
+        List<GrantedAuthority> authorities = paciente
+                .getPerfiles()
+                .stream()
+                .map(perfil ->
+                        new SimpleGrantedAuthority(perfil.getName().name())
+                        )
+                .collect(Collectors.toList());
+
+        PacienteDetailsImpl pacienteAuth = new PacienteDetailsImpl();
+        pacienteAuth = mappingPaciente(pacienteAuth,  paciente, authorities);
+
+        return pacienteAuth;
+    }
 
     /**
      * Indicates whether the user's account has expired. An expired account cannot be
@@ -67,5 +94,36 @@ public class PacienteDetailsImpl implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) { return true; }
+        if(o == null || getClass() != o.getClass()) { return false; }
+
+        PacienteDetailsImpl that = (PacienteDetailsImpl) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    /**
+     * Metodo que mapea los datos de un paciente a una entidad de tipo
+     * PacienteDetailsImpl
+     * @param paAuth el paciente con auth
+     * @param pa el paciente del modelo
+     * @param authorities los permisos
+     * @return un paciente con auth
+     */
+    public static PacienteDetailsImpl mappingPaciente(PacienteDetailsImpl paAuth, Paciente pa,
+                                               List<GrantedAuthority> authorities) {
+        paAuth.setId(pa.getId());
+        paAuth.setEmail(pa.getEmail());
+        paAuth.setUsername(pa.getUsername());
+        paAuth.setPassword(pa.getPassword());
+        paAuth.setAuthorities(authorities);
     }
 }
