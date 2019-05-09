87e6749c6ee7368229bed87bfe2a1ddce381a555
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-09 03:54:28 -0500

    JwtToken provided: methods implemented

--- src/main/java/com/mediworld/mwuserapi/security/JwtTokenProvider.java
+++ src/main/java/com/mediworld/mwuserapi/security/JwtTokenProvider.java
@@ -1,11 +1,13 @@
 package com.mediworld.mwuserapi.security;
 
-import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * <h1>JwtTokenProvider</h1>
@@ -29,8 +31,18 @@ public class JwtTokenProvider {
      * @return
      */
     public String generateToken(Authentication authentication) {
-        PacientePrincipal pacienteDetails = (PacientePrincipal) authentication.getPrincipal();
-        return null;
+        PacientePrincipal pacientePrincipal = (PacientePrincipal) authentication.getPrincipal();
+        // integridad del token respecto al tiempo
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + this.jwtExpirationInMs);
+
+        return Jwts.builder()
+                // mandando el id del paciente como body
+                .setSubject(pacientePrincipal.getId())
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecretKey)
+                .compact();
     }
 
     /**
@@ -39,15 +51,36 @@ public class JwtTokenProvider {
      * @return
      */
     public String getPacienteIdFromJWT(String token) {
-        return null;
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecretKey)
+                .parseClaimsJws(token)
+                // obteniendo el id del paciente
+                .getBody();
+
+        return claims.getSubject();
     }
 
     /**
      * metodo que valida la autenticidad de un token asi como su expiracion
+     * en caso de errores escribe al logger
      * @param authToken
      * @return
      */
     public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecretKey).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException ex) {
+            logger.error("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            logger.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty.");
+        }
         return false;
     }
 }
