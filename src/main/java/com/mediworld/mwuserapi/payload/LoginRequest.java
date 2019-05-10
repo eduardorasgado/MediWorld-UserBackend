d654660663374ea8be19f5bc7d642621c6052f36
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-10 04:52:05 -0500

    signup and login request type payloads

--- src/main/java/com/mediworld/mwuserapi/payload/LoginRequest.java
+++ src/main/java/com/mediworld/mwuserapi/payload/LoginRequest.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 
 /**
+ * <h1>LoginRequest</h1>
+ *
+ * Payload de tipo Request
  * Clase que representa un payload para la peticion a la api, para el login
  *
  * @author Eduardo Rasgado Ruiz
