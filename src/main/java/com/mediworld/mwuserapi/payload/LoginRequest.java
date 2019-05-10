0ed0de12fd82d49c9364ddd0b69b301be69534a2
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-10 04:44:45 -0500

    login request payload created.

---
+++ src/main/java/com/mediworld/mwuserapi/payload/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.mediworld.mwuserapi.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Clase que representa un payload para la peticion a la api, para el login
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
