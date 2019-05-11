d0bfb50af745a20faf342112b5d035833bdfe2e1
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-11 04:18:00 -0500

    Auth Rest Controller: registerPaciente method in process. mappingPaciente is implemented.

--- src/main/java/com/mediworld/mwuserapi/controller/AuthController.java
+++ src/main/java/com/mediworld/mwuserapi/controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.mediworld.mwuserapi.controller;
 
+import com.mediworld.mwuserapi.model.Genero;
+import com.mediworld.mwuserapi.model.Paciente;
+import com.mediworld.mwuserapi.payload.ApiResponse;
 import com.mediworld.mwuserapi.payload.JwtAuthenticationResponse;
 import com.mediworld.mwuserapi.payload.LoginRequest;
+import com.mediworld.mwuserapi.payload.SignUpRequest;
 import com.mediworld.mwuserapi.security.JwtTokenProvider;
 import com.mediworld.mwuserapi.services.IPacienteService;
 import com.mediworld.mwuserapi.services.IPerfilService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -50,7 +55,7 @@ public class AuthController {
      * @param loginRequest
      * @return
      */
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseEntity<?> authenticatePaciente(
             @Valid @RequestBody LoginRequest loginRequest) {
 
@@ -68,6 +73,57 @@ public class AuthController {
         // asignando un token al paciente logueado
         String jwt = jwtTokenProvider.generateToken(authentication);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    /**
+     * Metodo para manejar una peticion de registro de un nuevo paciente desde el lado del
+     * frontend.
+     * @param signUpRequest los datos del nuevo paciente
+     * @return
+     */
+    @PostMapping("/register")
+    public ResponseEntity<?> registerPaciente(
+            @Valid @RequestBody SignUpRequest signUpRequest) {
+
+        // validando disponibilidad de el username
+        if(pacienteService.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity<>(new ApiResponse(
+                    false, "El nombre de usuario ya está en uso"
+            ), HttpStatus.BAD_REQUEST);
+        }
+
+        // validando disponibilidad de el correo
+        if(pacienteService.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity<>(new ApiResponse(
+                    false, "El correo electrónico ya está registrado"
+            ), HttpStatus.BAD_REQUEST);
+        }
+
+        // Creando la nueva cuenta
+        Paciente paciente = new Paciente();
+
+        return null;
+    }
+
+    public Paciente mappingPaciente(Paciente paciente, SignUpRequest pacienteVO) {
+        paciente.setUsername(pacienteVO.getUsername());
+        paciente.setEmail(pacienteVO.getEmail());
+        paciente.setPassword(passwordEncoder.encode(pacienteVO.getPassword()));
+        paciente.setNombre(pacienteVO.getNombre());
+        paciente.setApellidos(pacienteVO.getApellidos());
+        paciente.setFechaNacimiento(pacienteVO.getFechaNacimiento());
+        String genero = pacienteVO.getGenero();
+        if(!genero.isEmpty()) {
+            if(genero.equals(Genero.HOMBRE) ||
+                    genero.equals(Genero.MUJER)){
+                if(genero.equals(Genero.HOMBRE)){
+                    paciente.setGenero(Genero.HOMBRE);
+                } else {
+                    paciente.setGenero(Genero.MUJER);
+                }
+            }
+        }
+        return paciente;
     }
 
 }
