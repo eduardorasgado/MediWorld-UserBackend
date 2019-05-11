7bacbc0e278ed620db0c23ad66b6029653ba916f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-11 06:17:51 -0500

    util package created. app constants class created.

---
+++ src/main/java/com/mediworld/mwuserapi/util/AppConstants.java
@@ -0,0 +1,16 @@
+package com.mediworld.mwuserapi.util;
+
+/**
+ * <h1>AppConstants</h1>
+ * Clase que mapean todas las constantes que son utilizadas en toda la aplicacion
+ * con el fin de evitar errores sintactivos asi mismo de escritura de estas constantes
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+public class AppConstants {
+
+    public static final String HOMBRE = "HOMBRE";
+
+    public static final String MUJER = "MUJER";
+
+}
