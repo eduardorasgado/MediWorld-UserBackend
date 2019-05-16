8df7e39ad3d35391d05754256b703fa8717f404b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-16 18:23:05 -0500

    new user availability class to send it to as api response.

---
+++ src/main/java/com/mediworld/mwuserapi/payload/UserAuthDataAvailability.java
@@ -0,0 +1,18 @@
+package com.mediworld.mwuserapi.payload;
+
+import lombok.Data;
+
+/**
+ * Clase que permite enviar la disponibilidad de nombre de usuario y correo electronico de un
+ * paciente a la api
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+@Data
+public class UserAuthDataAvailability {
+    private Boolean available;
+
+    public UserAuthDataAvailability(Boolean available) {
+        this.available = available;
+    }
+}
