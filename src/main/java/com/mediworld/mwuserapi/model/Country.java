60d397816541f90c309c7ffd173aca000b9c7399
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-25 21:44:12 -0500

    Country model and country names enum created. To be able to use it into medico and paciente models

---
+++ src/main/java/com/mediworld/mwuserapi/model/Country.java
@@ -0,0 +1,44 @@
+package com.mediworld.mwuserapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+/**
+ * <h1>Country</h1>
+ * Clase que va a manejar el nombre por enumeracion de cada posible pais,
+ * manejará posibles normas legales o nomenclaturas
+ *
+ * @author Eduardo Rasgado Ruiz
+ */
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid2")
+    private String id;
+
+    @NotBlank
+    @Column(length = 74)
+    //el nombre del pais mas largo es libia en su nombre nativo con 74 caracteres
+    private CountryName name;
+
+    @OneToMany(mappedBy = "paisResidencia", fetch = FetchType.EAGER)
+    private Set<Paciente> nacimientoPaciente;
+    @OneToMany(mappedBy = "paisNacimiento", fetch = FetchType.EAGER)
+    private Set<Paciente> residenciaPaciente;
+
+    @OneToMany(mappedBy = "paisResidencia", fetch = FetchType.EAGER)
+    private Set<Medico> nacimientoMedico;
+    @OneToMany(mappedBy = "paisNacimiento", fetch = FetchType.EAGER)
+    private Set<Medico> residenciaMedico;
+
+}
