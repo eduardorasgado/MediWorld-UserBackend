422ece8e0b63f4bfb0a4dc4da652a0f1bd28b5de
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-02 02:26:35 -0500

    login request payload renamed to paciente login request. Medico login request created.

---
+++ src/main/java/com/mediworld/mwuserapi/payload/MedicoLoginRequest.java
@@ -0,0 +1,21 @@
+package com.mediworld.mwuserapi.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * Payload que se trae la informacion que trae el request lo login del medico
+ *
+ * @author Eduardo Rasgado Ruiz
+ * @see com.mediworld.mwuserapi.controller.MedicoAuthController
+ */
+@Data
+public class MedicoLoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+}
--- src/main/java/com/mediworld/mwuserapi/payload/PacienteLoginRequest.java
+++ src/main/java/com/mediworld/mwuserapi/payload/PacienteLoginRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 
 /**
- * <h1>LoginRequest</h1>
+ * <h1>PacienteLoginRequest</h1>
  *
  * Payload de tipo Request
  * Clase que representa un payload para la peticion a la api, para el login
@@ -13,7 +13,7 @@ import javax.validation.constraints.NotBlank;
  * @author Eduardo Rasgado Ruiz
  */
 @Data
-public class LoginRequest {
+public class PacienteLoginRequest {
     @NotBlank
     private String username;
     @NotBlank
