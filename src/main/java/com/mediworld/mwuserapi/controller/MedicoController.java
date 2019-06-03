2cd73b8061e36d092985088589d508d47f7ec9a4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-03 00:17:18 -0500

    In Medico Controller getCurrentMedico method implementation. PreAutorize annotations.

--- src/main/java/com/mediworld/mwuserapi/controller/MedicoController.java
+++ src/main/java/com/mediworld/mwuserapi/controller/MedicoController.java
@@ -1,12 +1,14 @@
 package com.mediworld.mwuserapi.controller;
 
 import com.mediworld.mwuserapi.model.Medico;
+import com.mediworld.mwuserapi.payload.MedicoProfile;
 import com.mediworld.mwuserapi.payload.UserAuthDataAvailability;
+import com.mediworld.mwuserapi.security.CurrentUsuario;
+import com.mediworld.mwuserapi.security.MedicoPrincipal;
 import com.mediworld.mwuserapi.services.IMedicoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <h1>MedicoController</h1>
@@ -29,8 +31,26 @@ public class MedicoController {
      * @return
      */
     @GetMapping("/me")
-    public Medico getCurrentMedico() {
-        System.out.println("Youre calling [Medico Rest Controller]");
+    @PreAuthorize("hasAuthority('MEDICO') or hasAuthority('MEDICO_ACTIVE')")
+    public MedicoProfile getCurrentMedico(
+            @CurrentUsuario MedicoPrincipal currentMedico
+            ) {
+
+        MedicoProfile medicoProfile = new MedicoProfile(
+                currentMedico.getId(),
+                currentMedico.getEmail(),
+                currentMedico.getNombre(),
+                currentMedico.getApellidos()
+        );
+        return medicoProfile;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('MEDICO') or hasAuthority('MEDICO_ACTIVE')")
+    public ResponseEntity<MedicoProfile> getMedicoProfile(
+            @PathVariable(value = "id") String idMedico
+    ) {
+        System.out.println("Id medico: "+idMedico);
         return null;
     }
 
